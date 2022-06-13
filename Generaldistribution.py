943fbd12b73256a8b1cf71a460296a409eecad29
vpontello <vpontello@outlook.com>
2022-06-13 15:14:46 -0300

    modularized code

---
+++ Generaldistribution.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Distribution():
+    """ Generic distribution class for calculating and 
+    visualizing a Gaussian distribution.
+    
+    Attributes:
+        mean (float) representing the mean value of the distribution
+        stdev (float) representing the standard deviation of the distribution
+        data_list (list of floats) a list of floats extracted from the data file
+            
+    """
+    def __init__(self, mu=0, sigma=1):
+        self.mean = mu
+        self.stdev = sigma
+        self.data = np.array([])
+
+    def read_data_file(self, file_name, sample=True):
+    
+        """Method to read in data from a txt file. The txt file should have
+        one number (float) per line. The numbers are stored in the data attribute. 
+        After reading in the file, the mean and standard deviation are calculated
+                
+        Args:
+            file_name (string): name of a file to read from
+        
+        Returns:
+            None
+        
+        """
+        
+        # This code opens a data file and appends the data to a list called data_list
+        with open(file_name) as file:
+            data_list = []
+            line = file.readline()
+            while line:
+                data_list.append(int(line))
+                line = file.readline()
+        file.close()
+
+        self.data = np.array(data_list)
